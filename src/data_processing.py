548482e8857b85642ed0e1fb09d013382ca99bc6
wilhayla <wilhayla@hotmail.com>
2025-12-10 20:23:49 -0300

    mover variable DATE_FORMAT fuera de la funcion (variable global)

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,17 +1,19 @@
+# En src/data_processing.py
+
 import pandas as pd
 
+# 1. DEFINIR LA CONSTANTE FUERA DEL CUERPO DE LA FUNCIÓN
+#    Así es accesible inmediatamente y es una constante del módulo.
+DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+
 def convert_dates(df: pd.DataFrame, date_cols: list) -> pd.DataFrame:
-
-    '''Convierte una lista de columnas a formato datetime de Pandas.'''
-
-    DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
-
+    """Convierte una lista de columnas a formato datetime de Pandas, especificando el formato."""
+    
     for col in date_cols:
         if col in df.columns:
             df[col] = pd.to_datetime(
                 df[col], 
-                format=DATE_FORMAT,
-                errors='coarce'
-                )
-
-    return df 
+                format=DATE_FORMAT, # <-- Se usa la constante definida arriba
+                errors='coerce'
+            )
+    return df
