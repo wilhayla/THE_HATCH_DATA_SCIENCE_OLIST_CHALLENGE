529373b6c38aa5e9992de3f623be6c3ddf258ff1
wilhayla <wilhayla@hotmail.com>
2025-12-10 21:14:05 -0300

    correccion funcion convert_dates

--- src/data_processing.py
+++ src/data_processing.py
@@ -4,8 +4,14 @@ def convert_dates(df: pd.DataFrame, date_cols: list) -> pd.DataFrame:
 
     '''Convierte una lista de columnas a formato datetime de Pandas.'''
 
+    DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
+
     for col in date_cols:
         if col in df.columns:
-            df[col] = pd.to_datetime(df[col], errors='coarce')
-            
-    return df
+            df[col] = pd.to_datetime(
+                df[col], 
+                format=DATE_FORMAT,
+                errors='coarce'
+                )
+
+    return df 
